0944ac63ef6329c8d130f537477d5d1fb3e4ef6b
Nemyr <antoshko@list.ru>
2020-09-08 15:34:24 +0300

    настроено подключение к базе

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
+    'dsn' => 'mysql:host=localhost;dbname=doctestDB',
+    'username' => 'ruser',
     'password' => '',
     'charset' => 'utf8',
 
